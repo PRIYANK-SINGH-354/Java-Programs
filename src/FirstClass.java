a907f8a6210d16c59d0b9f4661b5686f21ec49f7
Priyank-SINGH-354 <yadavpriyank00@gmail.com>
2024-01-23 13:27:52 +0530

    First class in java

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("This is the first class in java");
+        int a=10, b=5;
+        int c= a+b;
+        System.out.println("Sum of the numbers is: " +c);
     }
 }
