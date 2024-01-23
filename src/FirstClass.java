c311bface7f41e82f53b2f04a58d661174c055cb
Priyank-SINGH-354 <yadavpriyank00@gmail.com>
2024-01-23 13:32:27 +0530

    In this code we are giving two variable static input and printing the sum of it

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,6 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
         int a=10, b=5;
         int c= a+b;
-        System.out.println("Sum of the numbers is: " +c);
+        System.out.println("Sum of the numbers is: " + c);
     }
 }
