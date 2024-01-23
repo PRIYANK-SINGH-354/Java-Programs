cd11ac025f3b9986320e2409c8d15cab697fbc4f
Priyank-SINGH-354 <yadavpriyank00@gmail.com>
2024-01-23 13:39:30 +0530

    Performing basic arithmetic operations on two variables

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,6 +2,14 @@ public class FirstClass {
     public static void main(String[] args) {
         int a=10, b=5;
         int c= a+b;
+        int d= a-b;
+        int e= a*b;
+        int f= a/b;
+        int g= a%b;
         System.out.println("Sum of the numbers is: " + c);
+        System.out.println("Difference of the number is: " + d);
+        System.out.println("Multiplication of the number is: " + e);
+        System.out.println("Division of the number is: " + f);
+        System.out.println("Remainder when a is divided by b is: " + g);
     }
 }
