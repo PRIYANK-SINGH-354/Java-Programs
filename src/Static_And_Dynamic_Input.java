14153cfd52f86d735a2ec7fc1581b0b3d2bc9785
Priyank-SINGH-354 <yadavpriyank00@gmail.com>
2024-01-24 16:17:53 +0530

    Learning about static and dynamic input

---
+++ src/Static_And_Dynamic_Input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Static_And_Dynamic_Input {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String name= sc.next();
+        String name1= "Priyank";
+        System.out.println(name+ " is a dynamically Input name by the user");
+        System.out.println(name1 + " is a statically Input name by the coder");
+    }
+
+}
