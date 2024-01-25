b73c3211e088a2154aaef2cd2b9e0c4d33b05ba8
PRIYANK SINGH <114853793+PRIYANK-SINGH-354@users.noreply.github.com>
2024-01-25 22:33:51 +0530

    Create DataTypes.java
    Learning about basic data types in java

---
+++ src/DataTypes.java
@@ -0,0 +1,14 @@
+// 3. Learning about basic data types in Java
+public class DataTypes {
+    public static void main(String[] args) {
+        int integervalue = 10;
+        float floatvalue = 10;
+        double doublevalue = 10.0;
+        char charactervalue = 'A';
+
+        System.out.println("Integer data: " + integervalue);
+        System.out.println("Float data: " + floatvalue);
+        System.out.println("Double data: " + doublevalue);
+        System.out.println("Character data: " + charactervalue);
+    }
+}
