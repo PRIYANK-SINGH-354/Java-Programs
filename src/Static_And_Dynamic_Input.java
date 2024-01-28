0084ea1b79c5eba466359eb1d145603c0bd5af6a
PRIYANK SINGH <114853793+PRIYANK-SINGH-354@users.noreply.github.com>
2024-01-28 23:42:30 +0530

    Update Static_And_Dynamic_Input.java

--- src/Static_And_Dynamic_Input.java
+++ src/Static_And_Dynamic_Input.java
@@ -1,4 +1,4 @@
-// 2. Learning to take Dynamic Input from user and printing both dynamic and static input to com
+// 2. Learning to take Dynamic Input from user and printing both dynamic and static input to compare
 import java.util.*;
 public class Static_And_Dynamic_Input {
     public static void main(String[] args) {
