8b096b4e43d1f832370b2c6e3996c9228d3b31d7
PRIYANK SINGH <114853793+PRIYANK-SINGH-354@users.noreply.github.com>
2024-01-30 23:06:28 +0530

    Create If_Else.java

---
+++ src/If_Else.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class If_Else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age >= 18) {
+            System.out.println("You are an adult and eligible for vote");
+        }
+        else {
+            System.out.println("You are not an adult and not eligible for vote");
+        }
+    }
+}
